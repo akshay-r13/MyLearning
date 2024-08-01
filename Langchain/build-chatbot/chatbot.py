48a2840a23566da6c52996e1f632cfb70c489232
Akshay Raman <akshay.raman97@gmail.com>
2024-08-01 23:18:38 +0530

    added chatbot code

--- Langchain/build-chatbot/chatbot.py
+++ Langchain/build-chatbot/chatbot.py
@@ -1,9 +1,12 @@
 from langchain_groq import ChatGroq
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, trim_messages
 from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+from operator import itemgetter
 
 model  = ChatGroq(model="llama3-70b-8192")
 
@@ -14,7 +17,25 @@ prompt_template = ChatPromptTemplate(
     ]
 )
 
-chain = prompt_template | model | StrOutputParser()
+trimmer = trim_messages(
+
+    
+    max_tokens=65,
+    strategy="last",
+    token_counter=model,
+    include_system=True,
+    allow_partial=False,
+    start_on="human"
+)
+
+
+
+chain = (
+    RunnablePassthrough.assign(messages=itemgetter("messages") | trimmer)
+    | prompt_template
+    | model
+    | StrOutputParser()
+)
 
 store = {}
 
@@ -27,12 +48,37 @@ with_messages_history = RunnableWithMessageHistory(chain, get_session_history, i
 
 configuration = {"configurable": {"session_id": "test-1"}}
 
+messages = [
+    SystemMessage(content="you're a good assistant"),
+    HumanMessage(content="hi! I'm bob"),
+    AIMessage(content="hi!"),
+    HumanMessage(content="I like vanilla ice cream"),
+    AIMessage(content="nice"),
+    HumanMessage(content="whats 2 + 2"),
+    AIMessage(content="4"),
+    HumanMessage(content="thanks"),
+    AIMessage(content="no problem!"),
+    HumanMessage(content="having fun?"),
+    AIMessage(content="yes!"),
+]
+
 response = with_messages_history.invoke(
         {
                 "language": "Spanish",
-                "messages": [HumanMessage("My name is Bob"), AIMessage("Nice to meet you"), HumanMessage("What's your name")]
+                "messages": messages + [HumanMessage(content="what's my name?")]
         },
         config=configuration
     )
 
 print(response)
+
+config = {"configurable": {"session_id": "abc15"}}
+
+for r in with_messages_history.stream(
+    {
+        "messages": [HumanMessage(content="hi! I'm todd. tell me a joke")],
+        "language": "English",
+    },
+    config=config,
+):
+    print(r, end="")
