b724ebbdd0672b4c11dd4976307aa544dc9b978e
Akshay Raman <akshay.raman97@gmail.com>
2024-08-04 00:45:32 +0530

    added vector store and retrievers

---
+++ LangchainLearning/official-docs-tutorials/build-vector-stores-and-retrievers.py
@@ -0,0 +1,42 @@
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+hf = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+
+documents = [
+    Document(
+        page_content="Dogs are great companions, known for their loyalty and friendliness.",
+        metadata={"source": "mammal-pets-doc"},
+    ),
+    Document(
+        page_content="Cats are independent pets that often enjoy their own space.",
+        metadata={"source": "mammal-pets-doc"},
+    ),
+    Document(
+        page_content="Goldfish are popular pets for beginners, requiring relatively simple care.",
+        metadata={"source": "fish-pets-doc"},
+    ),
+    Document(
+        page_content="Parrots are intelligent birds capable of mimicking human speech.",
+        metadata={"source": "bird-pets-doc"},
+    ),
+    Document(
+        page_content="Rabbits are social animals that need plenty of space to hop around.",
+        metadata={"source": "mammal-pets-doc"},
+    ),
+]
+
+vectorstore = Chroma.from_documents(
+    documents,
+    embedding=HuggingFaceEmbeddings(),
+)
+
+print(vectorstore.similarity_search("cat"))
